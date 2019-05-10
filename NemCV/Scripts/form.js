71b45f49891e69e67090b56884879a8fc54f6551
Madswolf <mads@madswolf.dk>
2019-05-10 12:54:25 +0200

    made a collapsible webcomponent

--- NemCV/Scripts/form.js
+++ NemCV/Scripts/form.js
@@ -3,15 +3,15 @@ function doThing() {
 
     for (let i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function () {
-                    this.classList.toggle("active");
-                    let content = this.nextElementSibling;
-                    if (content.style.display === "block") {
-                        content.style.display = "none";
-                        content.style.height = "100%";
-                        this.style.width = "50%";
-                    } else {
-                        content.style.display = "block";
-                        this.style.width = "100%";
+            this.classList.toggle("active");
+            let content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+                content.style.height = "100%";
+                this.style.width = "50%";
+            } else {
+                content.style.display = "block";
+                this.style.width = "100%";
             }
         });
     }
--- NemCV/Scripts/form_webcomponent.js
+++ NemCV/Scripts/form_webcomponent.js
@@ -1,6 +1,11 @@
-class MyComponent extends HTMLElement {
+class FormWebcomponent extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = <h1>Hello world</h1>;
+        this.innerHTML = "<button class='collapsible'>hej med dig</button>" +
+        "<div class='content'>" +
+            "<p>lorem impsum dolor mucho bueno</p>" +
+        "</div>";
     }
 }
-customElements.define('my-component', MyComponent);
+customElements.define('form-component', FormWebcomponent);
+const element = document.createElement("form-component");
+const shadowRoot = element.attachShadow({mode: "open"});
