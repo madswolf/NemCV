1dc65459af3d97848b1184177a83590a5ac6f30f
Madswolf <mads@madswolf.dk>
2019-05-10 15:23:44 +0200

    did the awesome stuff and we were super cool, learned a lot of things and did the cool

--- NemCV/Scripts/form_webcomponent.js
+++ NemCV/Scripts/form_webcomponent.js
@@ -1,11 +1,24 @@
 class FormWebcomponent extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = "<button class='collapsible'>hej med dig</button>" +
-        "<div class='content'>" +
-            "<p>lorem impsum dolor mucho bueno</p>" +
-        "</div>";
+        this.innerHTML = "<link rel='stylesheet' href='./Styles/form.css' >" +
+            "<button class='collapsible'  onclick='showContent(this)' id='button'>hej med dig</button>" +
+            "<p class='content' id='content'>asdw</p>";
     }
 }
 customElements.define('form-component', FormWebcomponent);
 const element = document.createElement("form-component");
 const shadowRoot = element.attachShadow({mode: "open"});
+
+function showContent(collapsible) {
+    console.log(collapsible);
+    let content = collapsible.nextElementSibling;
+    console.log(content);
+    if (content.style.display === "block") {
+        content.style.display = "none";
+        content.style.height = "100%";
+        collapsible.style.width = "50%";
+    } else {
+        content.style.display = "block";
+        collapsible.style.width = "100%";
+    }
+}
