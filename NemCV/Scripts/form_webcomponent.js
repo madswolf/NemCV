0715b36501d0bfc755854d2ac2b7c5783fc42475
Madswolf <mads@madswolf.dk>
2019-05-11 11:40:33 +0200

    made slots work by putting stuff in the contructor

--- NemCV/Scripts/form_webcomponent.js
+++ NemCV/Scripts/form_webcomponent.js
@@ -1,31 +1,37 @@
 class FormWebcomponent extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = "<link rel='stylesheet' href='./Styles/form.css' >" +
-            "<button class='collapsible'  onclick='showContent(this)' id='button'>hej med dig</button>" +
-            "<div class='content' id='content'>" +
-            "<slot name='title'></slot>" +
-            "<slot name='form'></slot>" +
-            "<slot name='add-button'></slot>" +
-            "<button onclick='dontShowContent(this)'>OK</button>" +
-            "<button onclick='dontShowContent(this)'>CANCEL</button>" +
-            "</div>";
+    constructor(){
+        super();
+        const shadowRoot = this.attachShadow({mode: 'open'});
+        shadowRoot.innerHTML = `
+            <link rel='stylesheet' href='./Styles/form.css' >
+            <button class='collapsible'  onclick='showContent(this)' id='button'><slot name='title'></slot></button>
+            <div class='content' id='content'>
+                <h1 class='title'></h1>
+                <slot name='form'></slot>
+                <slot name='add-button'></slot>
+                <button onclick='dontShowContent(this)'>OK</button>
+                <button onclick='dontShowContent(this)'>CANCEL</button>
+            </div>`;
     }
 }
+
+
 customElements.define('form-component', FormWebcomponent);
 const element = document.createElement("form-component");
-const shadowRoot = element.attachShadow({mode: "open"});
 
 function showContent(collapsible) {
-    console.log(collapsible);
     let content = collapsible.nextElementSibling;
-    console.log(content);
+    let title = collapsible.firstChild;
+    console.log(title);
+    content.firstChild.innerHtml = title;
+
     collapsible.style.display = "none";
     content.style.display = "block";
     content.style.height = "100%";
 }
 
 function dontShowContent(goBackButton) {
-    var parent =goBackButton.parentElement;
+    var parent = goBackButton.parentElement;
     parent.style.display = "none";
     parent.previousElementSibling.style.display = "block";
 }
