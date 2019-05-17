824eb2caa755d72a7a0113cdbad24834c4eb75e5
Emil Haugaard Westendahl <emilwestendahl@gmail.com>
2019-05-17 10:50:37 +0200

    Working on API call to CV verify

--- NemCV/scripts/api.js
+++ NemCV/scripts/api.js
@@ -38,7 +38,56 @@ function dropdownError(){
     select.add(el);
 }
 
+function testCVValidation(){
+    const cv = {
+        "name": "Hest McHestert",
+        "age": 34,
+        "email": "hest@hestenettet.dk",
+        "city": "Hestved",
+        "picture": "http://hest.com/hest.jpg",
+        "description": "Håber jeg finder det fede arbejde",
+        "employers":[
+            {
+            "name": "Google",
+            "title": "Pedel",
+            "from": "2012"
+        },
+            {
+                "name": "Stalden",
+                "title": "Vallak",
+                "from": "2007-01-01",
+                "to": "2012-03-01"
+            }
+        ],"education": [
+            {
+                "name": "ITU",
+                "title": "cand.it",
+                "from": "2018",
+                "to": "2020"
+            }
+        ],
+        "sectors": [
+            "Transport", "Kultur og fritid"
+        ]
+    };
+    validateCV(cv);
+}
+
 async function validateCV(cv){
+    fetch(apicv, {
+        method: "POST",
+        body: JSON.stringify(cv),
+        headers:{
+            "content-type": "application/json"
+        }}).then(function(response){
+            if(response.ok === true){
+                alert("CV  modtaget")
+            } else{
+                console.log()
+            }
+
+            alert(response.status);
+    });
 
 }
 
