4f0d15f13178ffcc535f407dfc916f07610b18dc
Emil Haugaard Westendahl <emilwestendahl@gmail.com>
2019-05-21 11:21:36 +0200

    created education_form.js

---
+++ NemCV/scripts/education_form.js
@@ -0,0 +1,54 @@
+
+educationList = [];
+
+function createEducation(event){
+    console.log("Clicked");
+    event.preventDefault();
+    const eduPlace = document.getElementById("edu-place").value;
+    const eduType = document.getElementById("edu-type").value;
+    const eduStartYear = document.getElementById("edu-startyear").value;
+    const eduEndYear = document.getElementById("edu-endyear").value;
+
+    const education = {
+        "place": eduPlace,
+        "type": eduType,
+        "start": eduStartYear,
+        "end": eduEndYear
+    };
+
+    educationList.push(education);
+    updateEducationList();
+    document.getElementById("education-form").reset();
+}
+
+function updateEducationList(){
+    const ul = document.getElementById("education-list");
+    ul.innerHTML = "";
+
+    let counter = 0;
+    educationList.forEach(element => {
+        const newList = document.createElement("LI");
+        const textNode = document.createTextNode("Sted: " +
+            element.place + " Title: " + element.type + " År: " +
+            element.start + " - " + element.end);
+
+        const button = document.createElement("button");
+        button.innerHTML = "X";
+        button.setAttribute("id", "edu-delete-" + counter);
+        button.setAttribute("onclick", "deleteEducation(this.id)");
+
+        newList.appendChild(textNode);
+        newList.appendChild(button);
+        ul.appendChild(newList);
+
+        counter++;
+    })
+}
+
+function deleteEducation(id){
+    console.log(id);
+    educationList.splice(id.slice(11,12), 1);
+    updateEducationList();
+}
+
+
