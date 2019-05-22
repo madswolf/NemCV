97c1a0a115fcf2f13f0f8a99e4e8512af1f681fd
ripe <ripe@itu.dk>
2019-05-22 10:40:30 +0200

    Omg javascriptet virker for webcomponent nu for erfaring og uddannelse

--- NemCV/scripts/form_webcomponent.js
+++ NemCV/scripts/form_webcomponent.js
@@ -1,7 +1,7 @@
 class FormWebcomponent extends HTMLElement {
     constructor(){
         super();
-        const shadowRoot = this.attachShadow({mode: 'open'});
+        let shadowRoot = this.attachShadow({mode: 'open'});
         shadowRoot.innerHTML = `
             <link rel='stylesheet' href='./Styles/reset.css' >
             <link rel='stylesheet' href='./Styles/generic.css' >
@@ -17,12 +17,16 @@ class FormWebcomponent extends HTMLElement {
                 <button onclick='dontShowContent(this)'>CANCEL</button>
             </div>`;
     }
-}
 
+    getShadowRoot(){
+        return this.shadowRoot;
+    }
+}
 
 customElements.define('form-component', FormWebcomponent);
 const element = document.createElement("form-component");
 
+/*
 function getShadowRoot(element) {
     console.log(element);
     console.log(FormWebcomponent.shadowRoot);
@@ -31,7 +35,7 @@ function getShadowRoot(element) {
     }else{
         getShadowRoot(element.parentNode);
     }
-}
+}*/
 
 function showContent(collapsible) {
     console.log(collapsible.parentNode);
@@ -47,10 +51,87 @@ function showContent(collapsible) {
 
 function dontShowContent(goBackButton) {
     console.log(goBackButton.parentElement.parentNode);
-    var shadowRoot = goBackButton.parentElement.parentNode;
-    var collapsible = shadowRoot.getElementById("collapsible");
-    var content = shadowRoot.getElementById("content");
+    let shadowRoot = goBackButton.parentElement.parentNode;
+    let collapsible = shadowRoot.getElementById("collapsible");
+    let content = shadowRoot.getElementById("content");
     content.style.display = "none";
     collapsible.style.display = "block";
 }
 
+let eduList = [];
+let workList = [];
+
+function createAccomplishment(event){
+    event.preventDefault();
+   
+    const shadowRoot = event.target.parentNode.getShadowRoot();
+    console.log(shadowRoot);
+    console.log(shadowRoot.host);
+    console.log(shadowRoot.host.children[1]);
+    const formElement = shadowRoot.host.children[1];
+    const children  = formElement.children;
+    const   place   =  children[1].value;
+    const   type    =  children[3].value;
+    const startYear =  children[5].value;
+    const  endYear  =  children[7].value;
+
+    const accomplishment = {
+        "name" : place,
+        "title": type,
+        "from" : startYear,
+        "to"   : endYear
+    };
+
+    console.log(accomplishment);
+    if(formElement.id.includes("work")){
+        workList.push(accomplishment);
+        updateList("work", workList);
+    }else {
+        eduList.push(accomplishment);
+        updateList("education", eduList);
+    }
+    
+    formElement.reset();
+}
+
+function updateList(name, list){
+    const ul = document.getElementById(name + "-list");
+    let kind = "";
+    if(name == "work"){
+        kind = "Erfaring";
+    }else {
+        kind = "Uddannelse";
+    }
+    ul.innerHTML = "";
+
+    let counter = 0;
+    list.forEach(element => {
+        const newList = document.createElement("LI");
+        const textNode = document.createTextNode(
+            kind + ": " + element.name + ", " + element.title 
+            + " " + element.from + " - " + element.to);
+
+        const button = document.createElement("button");
+        button.innerHTML = "X";
+        button.setAttribute("id", name + "-delete-" + counter);
+        button.setAttribute("onclick", "deleteListElement(this.id)");
+
+        newList.appendChild(textNode);
+        newList.appendChild(button);
+        ul.appendChild(newList);
+
+        counter++;
+    })
+}
+
+function deleteListElement(id){
+    if(id.includes("work")){
+        workList.splice(id.slice(11,12), 1);
+        updateList("work", workList);
+    } else{
+        eduList.splice(id.slice(11,12), 1);
+        updateList("education", eduList);
+    }
+    
+    
+}
