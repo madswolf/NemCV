c65340c2d51ebcd3dd5536db5bf3110059d5dfd1
Emil Haugaard Westendahl <emilwestendahl@gmail.com>
2019-05-23 15:59:31 +0200

    Quick save - Added animation

--- NemCV/scripts/form_webcomponent.js
+++ NemCV/scripts/form_webcomponent.js
@@ -15,6 +15,7 @@ class FormWebcomponent extends HTMLElement {
                     justify-content: center;
                     align-items: center;
                     display: flex;
+                    margin-bottom: 10px;
                 }
                 button{
                     font-family: var(--headline-large);
@@ -31,6 +32,7 @@ class FormWebcomponent extends HTMLElement {
                     width: 400px;
                     margin-top: 10px;
                     }
+                
                     
                 button:hover{
                     background-color: rgba(255, 255, 255, 0.50);
@@ -42,6 +44,7 @@ class FormWebcomponent extends HTMLElement {
                 .button-primary{
                     border-color: var(--primary-color);
                     color: var(--primary-color);
+                    width: 125px;
                 }
                 
                 .button-primary:hover{
@@ -49,16 +52,48 @@ class FormWebcomponent extends HTMLElement {
                 }
                 
                 .content {
-                    width: 100%;
                     display: none;
                     overflow: hidden;
                     background-color: white;
+                    position:absolute;
+                    z-index:100;
+                    left:10%;
+                    top:10%;
+                    bottom: 10%;
+                    right: 10%;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                    border-radius: 50px;
+                    border-color: #03DAC6;
+                    border-style: solid;
+                    border-width: 5px;
+                }
+                
+                @keyframes scale-up-center {
+                    from {
+                      transform: scale(0.1);
+                      transform-origin: 50% 50%;
+                    }
+                    to {
+                      transform: scale(1);
+                      transform-origin: 50% 50%;
+                    }
+                    
+                }
+                
+                .scale-up-center {
+                animation-name: scale-up-center;
+                animation-duration: 0.5s;
+                animation-timing-function: cubic-bezier(0.390, 0.575, 0.565, 1.000);
+                animation-direction: normal;
+                animation-fill-mode: forwards;
                 }
             </style>
 
             
             <button onclick='showContent(this)' id='collapsible'><slot name='title' class='title' id='title'></slot></button>
-            <div class='content' id='content'>
+            <div class='content scale-up-center' id='content'>
                 <h1 class='title' id='contentTitle'></h1>
                 <slot name='form'></slot>
                 <slot name='add-button'></slot>
@@ -81,14 +116,11 @@ function showContent(collapsible) {
     let title = shadowRoot.getElementById("title").assignedElements()[0];
     let contentTitle = shadowRoot.getElementById("contentTitle");
     contentTitle.textContent = title.textContent;
-    collapsible.style.display = "none";
-    content.style.display = "block";
+    content.style.display = "flex";
 }
 
 function dontShowContent(goBackButton) {
     let shadowRoot = goBackButton.parentElement.parentNode;
-    let collapsible = shadowRoot.getElementById("collapsible");
     let content = shadowRoot.getElementById("content");
     content.style.display = "none";
-    collapsible.style.display = "inline-block";
 }
